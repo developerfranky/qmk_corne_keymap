0660c6fd6e8b09846f41da9fceaf3a9765ff3f38
Franky Rodriguez <developerfranky@gmail.com>
2020-08-30 22:56:14 -0400

    remove TAPPING_FORCE_HOLD

--- config.h
+++ config.h
@@ -32,7 +32,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL_PD2
 
-#define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 300
 #define AUTO_SHIFT_TIMEOUT 300
 
